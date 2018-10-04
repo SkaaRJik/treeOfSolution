fcd8df5523a5f6d93ea0c1b096fcca4fc6a28c4d
Filippov Victor <filippowvitia@yandex.ru>
2018-10-04 11:50:31 +0800

    Canvas added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,41 +2,72 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Controller {
     @FXML
     Accordion accordion;
 
-    public class Cube{
+    private class Cube{
         Color color;
-
-        public Cube(Color color) {
+        int x, y;
+        int level;
+        int size = 20;
+        public Cube(Color color, int x, int y, int level) {
             this.color = color;
+            this.x = x;
+            this.y = y;
+            this.level = level;
         }
 
         public Color getColor() {
             return color;
         }
+
+        public void putCubeUnder(Cube cube){
+            cube.x = this.x;
+            cube.y = this.y+size;
+        }
+
+        public void drawCube(GraphicsContext graphicsContext){
+            graphicsContext.setFill(color);
+            graphicsContext.fillRect(x, y,size,size);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public int getLevel() {
+            return level;
+        }
+
+        public int getSize() {
+            return size;
+        }
     }
+
 
 
     public void init(){
 
         Cube[] cubes = new Cube[6];
-        cubes[0] = new Cube(Color.DARKBLUE);
-        cubes[1] = new Cube(Color.RED);
-        cubes[2] = new Cube(Color.GREEN);
-        cubes[3] = new Cube(Color.PURPLE);
-        cubes[4] = new Cube(Color.LIGHTBLUE);
-        cubes[5] = new Cube(Color.ORANGE);
+        cubes[0] = new Cube(Color.DARKBLUE, 30, 10, 1);
+        cubes[1] = new Cube(Color.RED, 55, 10, 1);
+        cubes[2] = new Cube(Color.GREEN, 80, 10, 1);
+        cubes[3] = new Cube(Color.PURPLE, 105, 10, 1);
+        cubes[4] = new Cube(Color.LIGHTBLUE, 130, 10, 1);
+        cubes[5] = new Cube(Color.ORANGE, 155, 10, 1);
 
         HashMap<Integer, List<Cube>> em = new HashMap<>();
         HashMap<Integer, List<Cube>> er = new HashMap<>();
@@ -53,19 +84,35 @@ public class Controller {
         er.put(new Integer(3), new ArrayList<Cube>(1){{add(cubes[2]);}});
         er.put(new Integer(6), new ArrayList<Cube>(1){{add(cubes[5]);}});
 
+        HashSet<Cube> cubesSet ;
+
         if (er.size() > 1 || !em.isEmpty()) {
             for (Integer key : er.keySet()) {
+
+
+                Canvas canvas = new Canvas(300, 275);
+
+                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+                cubesSet = new HashSet<>(6);
+                for (int i = 0; i < cubes.length; i++) {
+                    /*if(er.get(i) != null )
+                        cubesSet.add(er.get(i).get(0));
+                    if(em.get(i) != null )
+                        cubesSet.add(em.get(i).get(0));*/
+                    cubes[i].drawCube(graphicsContext);
+                }
+
+                for (int i = 1; i <= 6; i++) {
+                    if (key != i) byPass(key, i, makeACopy(em), makeACopy(er), makeACopy(on), graphicsContext);
+                }
                 TitledPane pane = new TitledPane();
                 ScrollPane scrollPane = new ScrollPane();
-
-                Canvas canvas = new Canvas();
+                pane.setContent(scrollPane);
                 scrollPane.setContent(canvas);
-                for (int i = 1; i <= 6; i++) {
-                    if (key != i) byPass(key, i, makeACopy(em), makeACopy(er), makeACopy(on));
-                }
                 this.accordion.getPanes().add(pane);
             }
         }
+        this.accordion.getPanes().get(0);
 
     }
 
@@ -88,15 +135,14 @@ public class Controller {
 
 
     public boolean r1(int x, int y, HashMap<Integer, List<Cube>> em, HashMap<Integer, List<Cube>> er, HashMap<Integer, List<Cube>> on){
-        if(em.containsKey(y) & er.containsKey(y) & em.containsKey(x)){
+        if(em.containsKey(x) & er.containsKey(x) & em.containsKey(y)){
             on.put(x, new ArrayList<Cube>(2) {{add(er.get(x).get(0)); add(er.get(y).get(0));}});
+            on.get(x).get(0).putCubeUnder(on.get(x).get(1));
             em.remove(y);
             er.remove(x);
             return true;
         };
         return false;
-
-
     }
 
    /* public boolean c2(int x, int y, int z, HashMap<Integer, List<Cube>> em, HashMap<Integer, List<Cube>> er, HashMap<Integer, List<Cube>> on){
@@ -106,7 +152,7 @@ public class Controller {
     public int byPass(int x, int y,
                       HashMap<Integer, List<Cube>> em,
                       HashMap<Integer, List<Cube>> er,
-                      HashMap<Integer, List<Cube>> on){
+                      HashMap<Integer, List<Cube>> on, GraphicsContext graphicsContext){
 
 
 
@@ -118,19 +164,17 @@ public class Controller {
             HashMap<Integer, List<Cube>> copyEr = makeACopy(er);
             HashMap<Integer, List<Cube>> copyOn = makeACopy(on);
 
-            if(r1(x, y, copyEm, copyEr, copyOn)){
                 HashSet<Integer> freeKeys = new HashSet<>();
                 freeKeys.addAll(copyEm.keySet());
                 freeKeys.addAll(copyEr.keySet());
                 freeKeys.remove(y);
                 for (int key : freeKeys) {
-                    byPass(y, key, copyEm, copyEr, copyOn);
+                    byPass(y, key, copyEm, copyEr, copyOn, graphicsContext);
                 }
-
             }
 
 
-        }
+
 
         return 0;
 
@@ -145,7 +189,7 @@ public class Controller {
             List<Cube> list = entry.getValue();
             List<Cube> copyList = new ArrayList<>(list.size());
             for(Cube cube : list){
-                copyList.add(new Cube(cube.getColor()));
+                copyList.add(new Cube(cube.getColor(), cube.getX(), cube.getY(), cube.getLevel()));
             }
             copy.put(entry.getKey(), copyList);
         }
